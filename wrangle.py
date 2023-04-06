ca9e88be779d6424b99061a04f3a2133c276ff7a
Dionne Taylor <mrdionne.taylor@gmail.com>
2023-04-06 16:49:38 -0500

    wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -64,75 +64,22 @@ def get_data():
 
 
 # In[ ]:
-
-
-def get_titanic_data():
-    filename = "titanic.csv"
-
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
-        # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
-
-        # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_file(filename)
-
-        # Return the dataframe to the calling code
-        return df  
-
-
-# In[ ]:
-
-
-def get_iris_data():
-    filename = "iris.csv"
-
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
-        # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT * FROM species JOIN measurements USING (species_id)', get_connection('iris_db'))
-
-        # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_csv(filename)
-
-        # Return the dataframe to the calling code
-        return df  
-
-
-# In[ ]:
-
-
-def get_telco_data():
-    filename = "telco_churn.csv"
-
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
-        # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT * FROM customers LEFT JOIN contract_types USING (contract_type_id)LEFT JOIN internet_service_types USING (internet_service_type_id) LEFT JOIN payment_types USING (payment_type_id)', get_connection('telco_churn'))
-
-        # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_csv(filename)
-
-        # Return the dataframe to the calling code
-        return df  
     
 def get_zillow_data():
-    filename = "single_family_properties_2017.csv"
+    filename = "zillow_single_family_props_2017.csv"
 
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
         # read the SQL query into a dataframe
-        df = pd.read_sql("SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 LEFT JOIN propertylandusetype USING (propertylandusetypeid) WHERE propertylandusetypeid = '261' OR '279'", get_connection('zillow'))
+        df = pd.read_sql("SELECT propertylandusetypeid, id, parcelid, bedroomcnt, bathroomcnt, fireplacecnt, calculatedbathnbr, calculatedfinishedsquarefeet, fullbathcnt, garagecarcnt, garagetotalsqft, latitude, longitude, lotsizesquarefeet, regionidzip, taxvaluedollarcnt, roomcnt, yearbuilt, numberofstories, assessmentyear, landtaxvaluedollarcnt, structuretaxvaluedollarcnt, taxamount, propertylandusetype.propertylandusedesc FROM properties_2017 LEFT JOIN typeconstructiontype USING (typeconstructiontypeid) LEFT JOIN propertylandusetype USING (propertylandusetypeid) WHERE propertylandusedesc = 'Single Family Residential'",get_connection('zillow'))
 
         # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_csv(filename)
         
         # Return the dataframe to the calling code
         return df 
+    
 
 
 # In[ ]:
@@ -205,7 +152,8 @@ def get_categorical_columns(df):
     """
     object_columns = df.select_dtypes(include=['object']).columns.to_list()
     boolean_columns = df.select_dtypes(include=['bool']).columns.to_list()
-    categorical_columns = object_columns + boolean_columns
+    category_columns = df.select_dtypes(include=['category']).columns.to_list()
+    categorical_columns = object_columns + boolean_columns + category_columns
     return categorical_columns
 
 def get_features(df):
@@ -218,7 +166,7 @@ def get_features(df):
 # In[ ]:
 
 def split_data(df):
-    # Split the data into training, testing, and validation sets
+    #Split the data into training, testing, and validation sets
     train, test = train_test_split(df, test_size=0.2, random_state=123)
     train, validate = train_test_split(train, test_size=0.25, random_state=123)
 
@@ -227,7 +175,7 @@ def split_data(df):
 def summarize_data(df):
     summary = pd.DataFrame(df.dtypes, columns=['dtypes'])
     summary = summary.reset_index()
-    summary['column_name'] = summary['index']
+    summary['column_name'] = summary.index
     summary = summary[['column_name', 'dtypes']]
     summary['missing_values'] = df.isnull().sum().values
     summary['unique_values'] = df.nunique().values
@@ -241,51 +189,73 @@ def summarize_data(df):
     summary['max'] = df.max().values
     return summary
 
-def clean_zillow_data(df):  
-   # Fill missing values in the 'bathroomcnt' and 'bedroomcnt' columns with 0
-    df.loc[:, ['bathroomcnt', 'bedroomcnt']] = df[['bathroomcnt', 'bedroomcnt']].fillna(0)
-
-    # Fill missing values in the 'taxvaluedollarcnt' and 'taxamount' columns with 0
-    df.loc[:, ['taxvaluedollarcnt', 'taxamount']] = df[['taxvaluedollarcnt', 'taxamount']].fillna(0)
-    
-    # Remove rows with more than a certain percentage of NaN values, e.g., 50%
-    threshold = 0.5
-    df = df.dropna(thresh=int(threshold * len(df.columns)))
-    
-    # Drop rows where more than half of the columns are missing
-    df = df.dropna(thresh=int(df.shape[1] / 2))
-    # Rename columns
-    df = df.rename(columns={'calculatedfinishedsquarefeet': 'total_sqft', 'taxvaluedollarcnt': 'assessed_property_value', 'bathroomcnt': 'num_bathrooms', 'bedroomcnt': 'num_bedrooms', 'fips':'county', 'yearbuilt':'year_built', 'taxamount':'total_property_tax'})
-
-    df['assessed_property_value'] = df['assessed_property_value'].astype(float)
-    
-    # Convert the column to integer data type using .astype()
-    # Convert the column to integer data type using .astype()
-    #df['total_sqft'] = df['total_sqft'].astype(int)
-  
-    
-    df = df.dropna(subset=['total_sqft'])
-    df['total_sqft'] = df['total_sqft'].astype(int)
-    
-    df = df.dropna(subset=['county'])
-    df['county'] = df['county'].astype(int)
-    
-    df = df.dropna(subset=['year_built'])
-    df['year_built'] = df['year_built'].astype(int)
-  
-    # Drop a single column
-    df = df.drop('Unnamed: 0', axis=1)
-    
-    # Create the OrdinalEncoder object
-    #encoder = OneHotEncoder()
-
-    # Fit and transform the 'category' column
-    #encoded_column = encoder.fit_transform(df[['county']])
-
-    # Replace the original 'category' column with the encoded one
-    #df['county'] = encoded_column
+def clean_zillow_data(df):
    
+    # Rename columns
+    df = df.rename(columns={'propertylandusetypeid': 'property_type_id',
+                            'id': 'record_id',
+                            'bedroomcnt': 'num_bedrooms',
+                            'bathroomcnt': 'num_bathrooms',
+                            'fireplacecnt': 'num_fireplaces',
+                            'calculatedfinishedsquarefeet': 'total_sqft',
+                            'fullbathcnt': 'num_full_baths',
+                            'garagecarcnt': 'num_garage_cars',
+                            'garagetotalsqft': 'garage_sqft',
+                            'latitude': 'lat',
+                            'longitude': 'long',
+                            'lotsizesquarefeet': 'lot_sqft',
+                            'regionidzip': 'zip_code',
+                            'taxvaluedollarcnt': 'assessed_property_value',
+                            'roomcnt': 'num_rooms',
+                            'yearbuilt': 'year_built',
+                            'numberofstories': 'num_stories',
+                            'assessmentyear': 'assessment_year',
+                            'landtaxvaluedollarcnt': 'land_tax_value',
+                            'structuretaxvaluedollarcnt': 'structure_tax_value',
+                            'taxamount': 'tax_amount'})
+
+    # Drop multiple columns at once
+    df = df.drop(['Unnamed: 0','calculatedbathnbr', 'num_garage_cars', 'num_stories', 
+                  'num_fireplaces', 'garage_sqft', 'propertylandusedesc', 'parcelid',                                   'structure_tax_value', 'land_tax_value', 'tax_amount', 'lat','long',                                   'property_type_id', 'assessment_year','num_bathrooms','num_bedrooms' ], axis=1)
+
+    # Reset indexes
+    df = df.set_index('record_id')
+    df = df.reset_index('record_id')
+    
+    # Convert columns to appropriate data types
+    df['assessed_property_value'] = df['assessed_property_value'].astype(float)
+    df['total_sqft'] = df['total_sqft'].astype(int)
+    df['year_built'] = df['year_built'].astype(int)
+    df['lot_sqft'] = df['lot_sqft'].astype(int)
+
     return df
+
+def drop_missing_data(df, threshold):
+    
+    #Drops rows from the dataframe where the percentage of missing values is above the thre
+    #Args:
+    #df (pd.DataFrame): the dataframe to process.
+    #threshold (float): the percentage threshold above which rows will be dr
+    #Returns:
+    #pd.DataFrame: the processed dataframe.
+    
+    # Calculate the number of non-missing values in each row.
+    num_non_missing = df.count(axis=1)
+    
+    # Calculate the percentage of missing values in each row.
+    pct_missing = 1 - num_non_missing / len(df.columns)
+ 
+    # Drop rows where the percentage of missing values is above the threshold.
+    df = df.loc[pct_missing <= threshold]
+ 
+    #df = drop_missing_data(df, 0.5)
+    return df
+
+def drop_all_null(df):
+    df = df.dropna()
+    df = df.dropna(how='all')
+    return df
+
 
 def remove_outliers_iqr(df, multiplier=1.5):
     # Select only numeric columns
@@ -307,29 +277,91 @@ def remove_outliers_iqr(df, multiplier=1.5):
     
     return df
 
+def remove_outliers_iqr_loop(df, multiplier=1.5):
+    # Select only numeric columns
+    num_cols = df.select_dtypes(include=np.number).columns.tolist()
+    
+    # Initialize the number of outliers
+    num_outliers = 1
+    
+    while num_outliers > 0:
+        # Initialize the number of outliers
+        num_outliers = 0
+        
+        # Loop through numeric columns
+        for col in num_cols:
+            # Calculate the IQR of the column
+            Q1 = df[col].quantile(0.25)
+            Q3 = df[col].quantile(0.75)
+            IQR = Q3 - Q1
+
+            # Define the upper and lower bounds for outliers
+            lower_bound = Q1 - multiplier * IQR
+            upper_bound = Q3 + multiplier * IQR
+
+            # Filter out the outliers from the DataFrame
+            num_outliers += df[col][(df[col] < lower_bound) | (df[col] > upper_bound)].count()
+            df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
+    
+    return df
+
 def standardize_text(df, columns):
     for col in columns:
         df[col] = df[col].str.upper().str.strip()
     return df
 
 
-def scale_data(df):
+def scale_x_data(df):
     # Create an instance of MinMaxScaler
     scaler = MinMaxScaler()
 
     # Fit the scaler to the 'county' column
-    scaler.fit(df[['county']])
+    scaler.fit(df[['total_sqft']])
 
     # Transform the 'county' column using the fitted scaler
-    df['county_scaled'] = scaler.transform(df[['county']])
+    df['total_sqft_scaled'] = scaler.transform(df[['total_sqft']])
+  
     return df
 
-def split_data(X, y):
+def scale_y_data(df):
+    # Create an instance of MinMaxScaler
+    scaler = MinMaxScaler()
+
+    # Fit the scaler to the 'county' column
+    scaler.fit(df[['assessed_property_value']])
+
+    # Transform the 'county' column using the fitted scaler
+    df['assessed_property_value_scaled'] = scaler.transform(df[['assessed_property_value']])
+    return df
+
+def scale_data(df):
+    # Create an instance of MinMaxScaler
+    scaler = MinMaxScaler()
+
+    # Fit the scaler to all features except the target variable
+    scaler.fit(df.drop(['assessed_property_value'], axis=1))
+
+    # Transform all features except the target variable using the fitted scaler
+    df[df.drop(['assessed_property_value'], axis=1).columns] = scaler.transform(df.drop(['assessed_property_value'], axis=1))
+
+    return df
+
+def define_X_y(df, target_col):
+    X = df.drop(columns=[target_col])
+    y = df[target_col].values
+    return X, y
+
+#X, y = define_X_y(df, 'assessed_property_value')
+#X = X[['total_sqft', 'lot_sqft', 'num_rooms', 'year_built']]
+
+
+#def split_data(df):
+    
     # First, split the data into train (80%) and a temporary set (20%)
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
+    #X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Next, split the temporary set into validate (10% of the original data) and test sets (10% of the original data)
-    X_validate, X_test, y_validate, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+    #X_validate, X_test, y_validate, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
     
     return X_train, X_validate, X_test, y_train, y_validate, y_test
 
@@ -343,60 +375,43 @@ def change_dtype(df, column_name, dtype):
 
 def wrangle_zillow_data():
     df = get_zillow_data()
+    df = drop_all_null(df)
     df = clean_zillow_data(df)
+    #df = drop_missing_data(df, 0.9)
+   
     return df
 
-def compare_scalers(df, features, target, test_size=0.2, random_state=42):
+def make_sample(df, frac=0.003):
+    """
+    This function takes a DataFrame as input and returns a random sample of the data.
     
-    # Define the target variable and features
-    target = 'total_property_tax'
-    features = [col for col in df.columns if col != target]
-    
-    X = df[features]
-    y = df[target]
+    Parameters
+    ----------
+    df : DataFrame
+        The input DataFrame to take a random sample from.
+    frac : float, optional
+        The fraction of the data to take as a sample. Default is 0.001 (0.1%).
+        
+    Returns
+    -------
+    DataFrame
+        The random sample of the input DataFrame.
+    """
+    sample_df = df.sample(frac=frac)
+    return sample_df
 
-    # Split the data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+# Example usage
+# sample_df = make_sample(df, frac=0.001)
 
-    # Define the scalers to test
-    scalers = {
-        'StandardScaler': StandardScaler(),
-        'MinMaxScaler': MinMaxScaler(),
-        'RobustScaler': RobustScaler(),
-        'MaxAbsScaler': MaxAbsScaler()
-    }
 
-    # Initialize a linear regression model
-    lm = LinearRegression()
-
-    results = []
-
-    # Iterate over each scaler, apply it to the data, and evaluate the model
-    for scaler_name, scaler in scalers.items():
-        # Scale the data
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
-
-        # Fit the model on the scaled data
-        lm.fit(X_train_scaled, y_train)
-
-        # Calculate the test mean squared error
-        y_pred = lm.predict(X_test_scaled)
-        mse = mean_squared_error(y_test, y_pred)
-        rmse = np.sqrt(mse)
-
-        # Store the results
-        results.append({
-            'Scaler': scaler_name,
-            'MSE': mse,
-            'RMSE': rmse
-        })
-
-    # Create a DataFrame with the results and return it
-    results_df = pd.DataFrame(results)
-    results_df = results_df.set_index('Scaler')
-
-    return results_df
+def count_outliers_iqr(df):
+    q1 = df.quantile(0.25)
+    q3 = df.quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    outliers = ((df < lower_bound) | (df > upper_bound)).sum().sum()
+    return outliers
 
 
 
